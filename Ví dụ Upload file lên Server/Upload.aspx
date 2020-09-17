48de04398c4f0ed1d27ae30fb6c1a22f48dcfe16
hoangtuyenblogger <hoangtuyenblogger@gmail.com>
2020-09-17 15:32:21 +0700

    Upload file lên Server ASP.NET

---
+++ Ví dụ Upload file lên Server/Upload.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Upload.aspx.cs" Inherits="HOANGTUYENMILKTEE.Upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload" runat="server" />
+            <asp:Button ID="btnGui" runat="server" Text="Gửi" OnClick="btnGui_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Ví dụ Upload file lên Server/Upload.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HOANGTUYENMILKTEE
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGui_Click(object sender, EventArgs e)
+        {
+            if(FileUpload.HasFile)//kiểm tra có tập tin nào được chọn không
+            {
+                //  tạo đường dẫn lưu tập tin
+                string duong_dan = "/App_data/" + FileUpload.FileName;
+
+                // lấy địa chỉ vật lý cho tập tin của ứng dụng
+                string dcvl = Server.MapPath(duong_dan);
+
+                // lưu tập tin vào địa chỉ vật lí
+                FileUpload.SaveAs(dcvl);
+
+                // thông báo đã lưu
+                Response.Write("\n Đã upload file " + FileUpload.FileName);
+                Response.Write("\n Đường dẫn của file là : "+dcvl);
+            }
+        }
+    }
+}
