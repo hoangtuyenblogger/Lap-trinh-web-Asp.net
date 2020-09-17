40ba46f90b513380763a16431a90362341845826
HOANG TUYEN <34962881+hoangtuyenblogger@users.noreply.github.com>
2020-09-17 14:20:05 +0700

    File xử lí sự kiện
    Xử lí sự kiện btnTao_session_Click

--- Session trong Asp.net/Tao_session.aspx.cs
+++ Session trong Asp.net/Tao_session.aspx.cs
@@ -11,7 +11,7 @@ namespace HOANGTUYENMILKTEE
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //
         }
 
         protected void btnTao_session_Click(object sender, EventArgs e)
